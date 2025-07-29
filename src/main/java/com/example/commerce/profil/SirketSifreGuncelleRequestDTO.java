c609690dc6f8c81169b8964a66848b3a461d9e0e
ahmet.celil <acyalman@gmail.com>
2025-07-29 23:02:05 +0300

    profil şifre değiştirme eklendi

---
+++ src/main/java/com/example/commerce/profil/SirketSifreGuncelleRequestDTO.java
@@ -0,0 +1,11 @@
+package com.example.commerce.profil;
+
+import com.example.commerce.basedtos.BaseRequestDto;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SirketSifreGuncelleRequestDTO {
+    private String yeniSirketParola;
+}
